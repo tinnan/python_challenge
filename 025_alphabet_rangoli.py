196c4061e05b75740b5a4c1563ca5a4ee8382fb4
tinnan <tin_nan@hotmail.com>
2017-03-30 20:56:11 +0700

    finish challenge.

--- 025_alphabet_rangoli.py
+++ 025_alphabet_rangoli.py
@@ -10,12 +10,28 @@ c-b-a-b-c
 ----c----
 """
 # unicode base
-base = 97
+base = 96  # 97 is 'a' in unicode
 
 
 def print_rangoli(size):
     line_size = 4*size - 3
+    plist = []
+    for i in range(size):  # i as running 0, 1, 2, 3, ... (also number of character on each line)
+        main_char = size - i
+        num, char_count = [], 2*i+1
+        mid = char_count // 2
+        for x in range(char_count):
+            # turn 0, 1, 2, 3, 4 -> 2, 1, 0, 1, 2
+            if x <= mid:  # left side
+                num.append(mid - x + main_char + base)
+            else:  # right side
+                num.append(main_char + x - mid + base)
 
+        plist.append(('{:c}-'*i + '{:c}' + '-{:c}'*i).format(*num))
+    for p in plist:
+        print(p.center(line_size, '-'))  # print top half
+    for p in reversed(plist[:len(plist)-1]):  # print bottom half as reversed list of the top half (minus the last line)
+        print(p.center(line_size, '-'))
 
 if __name__ == '__main__':
     n = int(input())
